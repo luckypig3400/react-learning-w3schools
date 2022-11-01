4a9408c9fa1370da69343a67f88013db7ec03708
郵差 <twHenryYang@gmail.com>
2022-11-01 17:12:31 +0800

    feat: practice ES6 Classes & Method in Classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const myFirstElement = <h1>Hello React~!</h1>
+const myFirstElement = <h1>Hello React~!</h1>;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(myFirstElement);
 
+class Car {
+  constructor(brandName) {
+    this.brand = brandName;
+  }
+
+  present() {
+    return "I have a " + this.brand;
+  }
+}
+
+const mycar = new Car("Toyota");
+console.log(mycar.present());
